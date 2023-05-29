4211977cf3c7d92c8e0bd9886cb52b903bd9c126
Liidiacnog <lilly22cn@gmail.com>
2023-05-29 18:13:30 +0200

    New KCSS with renamed fields and methods

--- kcss.h
+++ kcss.h
@@ -10,179 +10,163 @@
 class KCSS {
 private:
 
-	struct loc_t_base {
-		loc_t_base() noexcept :
-				tid(0), val(0) {
+	// ------------- loc_struct_base type: ------------- //
+
+	struct loc_struct_base {
+		loc_struct_base() noexcept :
+				snapshot_ts(0), value(0) {
 		}
-		loc_t_base(uint64_t v) noexcept :
-				tid(0), val(v) {
+		loc_struct_base(uint64_t v) noexcept :
+				snapshot_ts(0), value(v) {
 		}
-		uint64_t tid;
-		uint64_t val;
+		uint64_t snapshot_ts;
+		uint64_t value;
 	};
 
-	struct TagType {
-		uint64_t tagged :1;
-		uint64_t pid :15;
-		uint64_t tag :48;
-	};
-	static_assert( sizeof(TagType) == 8 );
+	// --------- management of TemporalValues: --------- //
 
-	constexpr inline bool tagged_id(uint64_t v) const noexcept {
+	struct TemporalValue {
+		uint64_t location_type  :1;
+		uint64_t thread_ID		:15;
+		uint64_t tag_number		:48;
+	};
+	static_assert( sizeof(TemporalValue) == 8 );
+
+	constexpr inline bool is_TemporalValue(uint64_t v) const noexcept {
 		return (v & 0x0000000000000001) == 1;
 	}
 
-	inline bool cas(uint64_t *p, uint64_t exp, uint64_t des) noexcept {
-		return __sync_bool_compare_and_swap(p, exp, des);
+	inline constexpr uint64_t make_TemporalValue(uint16_t thread_ID,
+		uint64_t tag_number) const noexcept {
+		union {
+			TemporalValue vt;
+			uint64_t a;
+		};
+
+		vt.thread_ID = thread_ID;
+		vt.tag_number = tag_number;
+		vt.location_type = 1;
+
+		return a;
 	}
 
-	constexpr inline uint16_t id(uint64_t v) const noexcept {
+	// ------------- thread ID methods: ------------- //
+
+	uint16_t my_thread_id() noexcept {
+		thread_local static uint16_t my_thread_id = _thread_id++;
+		return my_thread_id;
+	}
+
+	constexpr inline uint16_t thread_id(uint64_t v) const noexcept {
 		union {
-			TagType vt;
+			TemporalValue tv;
 			uint64_t a;
 		};
 		a = v;
-		return vt.pid;
-
+		
+		return tv.thread_ID;
 	}
 
-	inline void reset(loc_t_base *a) noexcept {
-		uint64_t oldval = a->val;
-		if (tagged_id(oldval)) {
-			cas(&a->val, oldval, VAL_SAVED[id(oldval)]);  // CAS
+	// ------------- methods for the KCSS algorithm: ------------- //
+
+	inline bool cas(uint64_t* loc_pointer, uint64_t exp, uint64_t new_val) noexcept {
+		return __sync_bool_compare_and_swap(loc_pointer, exp, new_val);
+	}
+
+	inline void reset(loc_struct_base *a) noexcept {
+		uint64_t old_val = a->value;
+		if (is_TemporalValue(old_val)) {
+			cas(&a->value, old_val, SAVED_VAL[thread_id(old_val)]);
 		}
 	}
 
-	inline uint64_t read(loc_t_base *a) noexcept {
+	inline uint64_t read(loc_struct_base *a) noexcept {
 		while (true) {
-			uint64_t val = a->val;
-			if (!tagged_id(val))
+			uint64_t val = a->value;
+			if (!is_TemporalValue(val))
 				return val;
 			reset(a);
 		}
 	}
 
-	inline constexpr uint64_t create_tagged_value(uint16_t pid,
-			uint64_t tag) const noexcept {
-		union {
-			TagType vt;
-			uint64_t a;
-		};
-
-		vt.pid = pid;
-		vt.tag = tag;
-		vt.tagged = 1;
-
-		return a;
-	}
-
-	uint16_t thread_id() noexcept {
-		thread_local static uint16_t id = _thread_id++;
-		return id;
-	}
-
-	inline uint64_t ll(loc_t_base *a) noexcept {
+	inline uint64_t ll(loc_struct_base *a) noexcept {
 		while (true) {
-			TAGS[thread_id()]++;
-			uint64_t oldVal = read(a);
-			VAL_SAVED[thread_id()] = oldVal;
-			uint64_t tag_id = create_tagged_value(thread_id(),
-					TAGS[thread_id()]);
-			if (cas(&a->val, oldVal, tag_id)) {
-				a->tid = tag_id;
-				return oldVal;
+			TAG_NUMBERS[my_thread_id()]++;
+			uint64_t old_val = read(a);
+			SAVED_VAL[my_thread_id()] = old_val;
+			uint64_t temp_val = make_TemporalValue(my_thread_id(),
+					TAG_NUMBERS[my_thread_id()]);
+			if (cas(&a->value, old_val, temp_val)) {
+				a->snapshot_ts = temp_val;
+				return old_val;
 			}
 		}
 	}
 
-	inline bool sc(loc_t_base *a, uint64_t newval) noexcept {
-		uint64_t tag_id = create_tagged_value(thread_id(), TAGS[thread_id()]);
-		return cas(&a->val, tag_id, newval);
+	inline bool sc(loc_struct_base *a, uint64_t new_prog_val) noexcept {
+		uint64_t temp_val = make_TemporalValue(my_thread_id(), TAG_NUMBERS[my_thread_id()]);
+		return cas(&a->value, temp_val, new_prog_val);
 	}
 
+	//          collect_snapshot_ts:          //
+
+	// Original method without iteration unfolding
+	inline constexpr void collect_snapshot_ts(const std::size_t k,
+			loc_struct_base **a, uint64_t *t) const noexcept {
+		for (unsigned int i = 0; i < k; ++i) {
+			t[i] = a[i]->snapshot_ts;
+		}
+	}
+
+	// Redefined method with iteration unfolding
 	template<std::size_t k>
-	inline constexpr void collect_tagged_ids_(loc_t_base **a,
-			uint64_t *ta) const noexcept {
-		collect_tagged_ids_<k - 1>(a, ta);
-		ta[k - 1] = a[k - 1]->tid;
+	inline constexpr void collect_snapshot_ts_(loc_struct_base **a,
+			uint64_t *t) const noexcept {
+		collect_snapshot_ts_<k - 1>(a, t);
+		t[k - 1] = a[k - 1]->snapshot_ts;
 	}
 
 	template<>
-	inline constexpr void collect_tagged_ids_<0>(loc_t_base**,
+	inline constexpr void collect_snapshot_ts_<0>(loc_struct_base**,
 			uint64_t*) const noexcept {
 	}
 
+	//          collect_values:          //
+	
+	// Original method without iteration unfolding
+	inline void collect_values(const std::size_t k, loc_struct_base **a,
+			uint64_t *v) noexcept {
+		for (unsigned int i = 0; i < k; ++i) {
+			v[i] = read(a[i]);
+		}
+	}
+	
+	// Redefined method with iteration unfolding
 	template<std::size_t k>
-	inline void collect_values_(loc_t_base **a, uint64_t *va) noexcept {
-		collect_values_<k - 1>(a, va);
-		va[k - 1] = read(a[k - 1]);
+	inline void collect_values_(loc_struct_base **a, uint64_t *v) noexcept {
+		collect_values_<k - 1>(a, v);
+		v[k - 1] = read(a[k - 1]);
 	}
 
 	template<>
-	inline void collect_values_<0>(loc_t_base**, uint64_t*) noexcept {
+	inline void collect_values_<0>(loc_struct_base**, uint64_t*) noexcept {
 	}
 
-	template<std::size_t k>
-	inline bool eval_cond_(uint64_t *va, uint64_t *vb, uint64_t *ta,
-			uint64_t *tb) noexcept {
-		return eval_cond_<k - 1>(va, vb, ta, tb) && ta[k - 1] == tb[k - 1]
-				&& va[k - 1] == vb[k - 1];
-	}
+	//          snapshot:          //
 
-	template<>
-	inline bool eval_cond_<0>(uint64_t*, uint64_t*, uint64_t*,
-			uint64_t*) noexcept {
-		return true;
-	}
-
-	template<std::size_t k>
-	inline void snapshot_(loc_t_base **a, uint64_t *va) noexcept {
-		uint64_t ta[k], tb[k];
-		uint64_t vb[k];
+	// Original method without iteration unfolding
+	inline void snapshot(std::size_t k, loc_struct_base **a, uint64_t *values_1) noexcept {
+		uint64_t timestamps_1[k], timestamps_2[k];
+		uint64_t values_2[k];
 
 		while (true) {
-			collect_tagged_ids_<k>(a, ta);
-			collect_values_<k>(a, va);
-			collect_values_<k>(a, vb);
-			collect_tagged_ids_<k>(a, tb);
-
-			if (eval_cond_<k>(va, vb, ta, tb))
-				return;
-		}
-
-		return;
-	}
-
-	template<>
-	inline void snapshot_<0>(loc_t_base**, uint64_t*) noexcept {
-	}
-
-	inline constexpr void collect_tagged_ids(const std::size_t k,
-			loc_t_base **a, uint64_t *ta) const noexcept {
-		for (unsigned int i = 0; i < k; ++i) {
-			ta[i] = a[i]->tid;
-		}
-	}
-
-	inline void collect_values(const std::size_t k, loc_t_base **a,
-			uint64_t *va) noexcept {
-		for (unsigned int i = 0; i < k; ++i) {
-			va[i] = read(a[i]);
-		}
-	}
-
-	inline void snapshot(std::size_t k, loc_t_base **a, uint64_t *va) noexcept {
-		uint64_t ta[k], tb[k];
-		uint64_t vb[k];
-
-		while (true) {
-			collect_tagged_ids(k, a, ta);
-			collect_values(k, a, va);
-			collect_values(k, a, vb);
-			collect_tagged_ids(k, a, tb);
+			collect_snapshot_ts(k, a, timestamps_1);
+			collect_values(k, a, values_1);
+			collect_values(k, a, values_2);
+			collect_snapshot_ts(k, a, timestamps_2);
 
 			unsigned i = 0;
-			while (i < k && ta[i] == tb[i] && va[i] == vb[i])
+			while (i < k && timestamps_1[i] == timestamps_2[i] && values_1[i] == values_2[i])
 				++i;
 
 			if (i == k)
@@ -192,10 +176,51 @@ private:
 		return;
 	}
 
+	// Redefined method with iteration unfolding
+
+	// eval_cond_ method for redefined snapshot_
 	template<std::size_t k>
-	inline bool eval_kcss_cond_(uint64_t *oldValues, uint64_t *expVals) noexcept {
-		return eval_kcss_cond_<k - 1>(oldValues, expVals)
-				|| oldValues[k - 1] != expVals[k - 1];
+	inline bool eval_cond_(uint64_t *values_1, uint64_t *values_2, uint64_t *timestamps_1,
+			uint64_t *timestamps_2) noexcept {
+		return eval_cond_<k - 1>(values_1, values_2, timestamps_1, timestamps_2) 
+					&& timestamps_1[k - 1] == timestamps_2[k - 1]
+					&& values_1[k - 1] == values_2[k - 1];
+	}
+
+	template<>
+	inline bool eval_cond_<0>(uint64_t*, uint64_t*, uint64_t*,
+			uint64_t*) noexcept {
+		return true;
+	}
+
+	// snapshot_ method
+	template<std::size_t k>
+	inline void snapshot_(loc_struct_base **a, uint64_t *values_1) noexcept {
+		uint64_t timestamps_1[k], timestamps_2[k];
+		uint64_t values_2[k];
+
+		while (true) {
+			collect_snapshot_ts_<k>(a, timestamps_1);
+			collect_values_<k>(a, values_1);
+			collect_values_<k>(a, values_2);
+			collect_snapshot_ts_<k>(a, timestamps_2);
+
+			if (eval_cond_<k>(values_1, values_2, timestamps_1, timestamps_2))
+				return;
+		}
+
+		return;
+	}
+
+	template<>
+	inline void snapshot_<0>(loc_struct_base**, uint64_t*) noexcept {
+	}
+
+	//      eval_kcss_cond_ method for redefined kcss:      //
+	template<std::size_t k>
+	inline bool eval_kcss_cond_(uint64_t *old_values, uint64_t *exp_values) noexcept {
+		return eval_kcss_cond_<k - 1>(old_values, exp_values)
+				|| old_values[k - 1] != exp_values[k - 1];
 	}
 
 	template<>
@@ -206,29 +231,29 @@ private:
 public:
 
 	KCSS() noexcept :
-			VAL_SAVED(), TAGS(), _thread_id() {
+			SAVED_VAL(), TAG_NUMBERS(), _thread_id() {
 	}
 
+	// ------------- loc_struct_t templates: ------------- //
+
 	template<typename T, class Enabled = void>
-	struct loc_t;
+	struct loc_struct_t;
 
-
-// uint64_t and int64_t
+	// loc_struct_t for uint64_t and int64_t
 	template<typename T>
-	struct loc_t<T,
-			std::enable_if_t<
-					std::is_same_v<T, uint64_t> || std::is_same_v<T, int64_t>>> : loc_t_base {
+	struct loc_struct_t<T, std::enable_if_t< std::is_same_v<T, uint64_t> || std::is_same_v<T, int64_t>>> : loc_struct_base {
+		
 		struct t63 {
-			T p :1;
-			T v :63;
+			T location_type :1;
+			T content :63;
 		};
 
-		loc_t() noexcept :
-				loc_t_base() {
+		loc_struct_t() noexcept :
+			loc_struct_base() {
 		}
 
-		loc_t(T v) noexcept :
-				loc_t_base(to_value_t(v)) {
+		loc_struct_t(T v) noexcept :
+			loc_struct_base(to_value_t(v)) {
 		}
 
 		constexpr inline uint64_t to_value_t(T v) const noexcept {
@@ -236,8 +261,9 @@ public:
 				t63 x;
 				uint64_t _x;
 			};
-			x.p = 0;
-			x.v = v;
+			x.location_type = 0;
+			x.content = v;
+
 			return _x;
 		}
 
@@ -247,31 +273,30 @@ public:
 				uint64_t _x;
 			};
 			_x = v;
-			return x.v;
-		}
 
+			return x.content;
+		}
 	};
 
-// any numeric type of size smaller than 8 bytes
+	// loc_struct_t for any numeric type of size smaller than 8bytes
 	template<typename T>
-	struct loc_t<T,
+	struct loc_struct_t<T,
 			std::enable_if_t<
 					(std::is_integral_v<T> || std::is_floating_point_v<T>)
-							&& (sizeof(T) < 8)>> : loc_t_base {
+							&& (sizeof(T) < 8)>> : loc_struct_base {
 
-		loc_t() noexcept :
-				loc_t_base() {
+		loc_struct_t() noexcept :
+				loc_struct_base() {
 		}
 
-		loc_t(T v) noexcept :
-				loc_t_base(to_value_t(v)) {
+		loc_struct_t(T v) noexcept :
+				loc_struct_base(to_value_t(v)) {
 		}
 
 		constexpr inline uint64_t to_value_t(T v) const noexcept {
-
 			union {
 				struct {
-					uint8_t p;
+					uint8_t location_type;
 					T a2;
 				} t;
 				uint64_t a1;
@@ -285,32 +310,33 @@ public:
 		constexpr inline T from_value_t(uint64_t v) const noexcept {
 			union {
 				struct {
-					uint8_t p;
+					uint8_t location_type;
 					T a2;
 				} t;
 				uint64_t a1;
 			};
 			a1 = v;
+
 			return t.a2;
 		}
-
 	};
 
-// Trying to handle double by stealing 1 bit from the mantissa. We steal the least
-// significant bit which is the one affects the precision less, since it contributes
-// 2^(-52) to the decimals -- see the following for more information:
-//
-//   https://en.wikipedia.org/wiki/Double-precision_floating-point_format
-//
+	// loc_struct_t for double:
+	// Trying to handle double by stealing 1 bit from the mantissa. We steal the least
+	// significant bit which is the one affects the precision less, since it contributes
+	// 2^(-53) to the decimals -- see the following for more information:
+	//
+	//   https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+	//
 	template<>
-	struct loc_t<double> : loc_t_base {
+	struct loc_struct_t<double> : loc_struct_base {
 
-		loc_t() noexcept :
-				loc_t_base() {
+		loc_struct_t() noexcept :
+				loc_struct_base() {
 		}
 
-		loc_t(double v) noexcept :
-				loc_t_base(to_value_t(v)) {
+		loc_struct_t(double v) noexcept :
+				loc_struct_base(to_value_t(v)) {
 		}
 
 		constexpr inline uint64_t to_value_t(double v) const noexcept {
@@ -322,7 +348,6 @@ public:
 			a2 = a2 & 0xfffffffffffffffe; // turn the least significant bit of the mantissa to 0
 
 			return a2;
-
 		}
 
 		constexpr inline double from_value_t(uint64_t v) const noexcept {
@@ -331,20 +356,21 @@ public:
 				double a1;
 			};
 			a2 = v;
+
 			return a1;
 		}
 	};
 
-// pointers
+	// loc_struct_t for pointers
 	template<typename T>
-	struct loc_t<T*> : loc_t_base {
+	struct loc_struct_t<T*> : loc_struct_base {
 
-		loc_t() noexcept :
-				loc_t_base() {
+		loc_struct_t() noexcept :
+				loc_struct_base() {
 		}
 
-		explicit loc_t(T *v) noexcept :
-				loc_t_base(to_value_t(v)) {
+		explicit loc_struct_t(T *v) noexcept :
+				loc_struct_base(to_value_t(v)) {
 		}
 
 		constexpr inline uint64_t to_value_t(T *v) const noexcept {
@@ -354,7 +380,8 @@ public:
 				uint64_t a;
 			};
 			v_aux = v;
-			assert((a & 1) == 0); // pointer must be aligned to even address
+			assert((a & 1) == 0);//pointer must be aligned to even address
+
 			return a;
 		}
 		constexpr inline T* from_value_t(uint64_t v) const noexcept {
@@ -363,49 +390,85 @@ public:
 				uint64_t v_aux;
 			};
 			v_aux = v;
+			
 			return a;
 		}
 	};
 
+	// ----------- auxiliary methods get() and mp(): ----------- //
+
 	template<typename T>
-	constexpr static std::pair<loc_t<T>&, T> mp(loc_t<T> &a, T b) noexcept {
-		return std::move(std::pair<loc_t<T>&, T>(a, b));
+	constexpr static std::pair<loc_struct_t<T>&, T> mp(loc_struct_t<T> &a, T b) noexcept {
+		return std::move(std::pair<loc_struct_t<T>&, T>(a, b));
 	}
 
 	template<typename T>
-	inline T get(loc_t<T> &a) noexcept {
+	inline T get(loc_struct_t<T> &a) noexcept {
 		return a.from_value_t(read(&a));
 	}
 
+	// ----------------- KCSS methods: ----------------- //
+
+	// Original kcss method without iteration unfolding
 	template<typename T0, typename ...Ts>
-	bool kcss(loc_t<T0> &a0, T0 a0_expv, T0 a0_newv, Ts &&...args) noexcept {
+	bool kcss(loc_struct_t<T0>& a0, T0 a0_expv, T0 a0_newv, Ts &&...args) noexcept {
 
 		constexpr std::size_t k = sizeof...(args) + 1;
 
-		uint64_t oldValues[k];
-		uint64_t expVals[k] = { a0.to_value_t(a0_expv),
+		uint64_t old_vals[k];
+		uint64_t exp_vals[k] = { a0.to_value_t(a0_expv),
 				(args.first.to_value_t(args.second))... };
-		loc_t_base *a[k] = { &a0, (&args.first)... };
-		uint64_t newVal = a0.to_value_t(a0_newv);
+		loc_struct_base* a[k] = { &a0, (&args.first)... };
+		uint64_t new_v = a0.to_value_t(a0_newv);
 
 		while (true) {
-			oldValues[0] = ll(a[0]);
-
-			snapshot_<k - 1>(a + 1, oldValues + 1);
-			if (eval_kcss_cond_<k>(oldValues, expVals)) {
-				sc(a[0], oldValues[0]);
-				return false;
+			old_vals[0] = ll(a[0]);
+			snapshot(k - 1, a + 1, old_vals + 1);
+			for (unsigned int i = 0; i < k; ++i) {
+				if (old_vals[i] != exp_vals[i]) {
+					sc(a[0], old_vals[0]);
+					return false;
+				}
 			}
 
-//			snapshot(k - 1, a + 1, oldValues + 1);
-//			for (unsigned int i = 0; i < k; ++i) {
-//				if (oldValues[i] != expVals[i]) {
-//					sc(a[0], oldValues[0]);
-//					return false;
-//				}
-//			}
+			if (sc(a[0], new_v)) {
+				return true;
+			}
+		}
 
-			if (sc(a[0], newVal)) {
+		return false;
+	}
+
+	// Redefined kcss method for iteration unfolding
+	template<typename T0, typename ...Ts>
+	bool kcss_(loc_struct_t<T0> &a0, T0 a0_expv, T0 a0_newv, Ts &&...args) noexcept {
+
+		constexpr std::size_t k = sizeof...(args) + 1;
+
+		uint64_t old_vals[k];
+		uint64_t exp_vals[k] = { a0.to_value_t(a0_expv),
+				(args.first.to_value_t(args.second))... };
+		loc_struct_base *a[k] = { &a0, (&args.first)... };
+		uint64_t new_v = a0.to_value_t(a0_newv);
+
+		while (true) {
+			old_vals[0] = ll(a[0]);
+
+			snapshot_<k - 1>(a + 1, old_vals + 1);
+			if (eval_kcss_cond_<k>(old_vals, exp_vals)) {
+				sc(a[0], old_vals[0]);
+				return false;
+			}
+			//The previous code block is equivalent to the following:
+			//	snapshot(k - 1, a + 1, old_vals + 1);
+			//	for (unsigned int i = 0; i < k; ++i) {
+			//		if (old_vals[i] != exp_vals[i]) {
+			//			sc(a[0], old_vals[0]);
+			//			return false;
+			//		}
+			//	}
+
+			if (sc(a[0], new_v)) {
 				return true;
 			}
 		}
@@ -417,10 +480,8 @@ private:
 	enum {
 		MAX_THREAD_ID = 100
 	};
-	uint64_t VAL_SAVED[MAX_THREAD_ID];
-	uint16_t TAGS[MAX_THREAD_ID];
+	uint64_t SAVED_VAL[MAX_THREAD_ID];
+	uint16_t TAG_NUMBERS[MAX_THREAD_ID];
 
 	std::atomic<uint16_t> _thread_id;
-
 };
-
--- main.cpp
+++ main.cpp
@@ -1,211 +1,172 @@
-#include <chrono>
-#include <iomanip>
 #include <iostream>
-
-#include "kcss.h"
+#ifndef kcss_h
+    #define kcss_h
+    #include "kcss.h"
+#endif
 #include <thread>
-#include <chrono>
 
-using std::__1::chrono::high_resolution_clock;
-using std::__1::chrono::microseconds;
-using namespace std::chrono_literals;
 
-void test0(std::size_t n) {
-	KCSS m;
+// ---------------------------------------------------------------------------------- //
 
-	KCSS::loc_t<int> v1(10);
-	KCSS::loc_t<int> v2(20);
-	KCSS::loc_t<int> v3(30);
+// Instantiate KCSS class
+KCSS kcss_instance;
 
+// Linked List definition: linked nodes using the following node structure
+struct node {
+	node():
+		x(0), next(nullptr) {
+	}
+	node(int _x_val, node* _next_val):
+		x(_x_val), next(_next_val) {
+	}
+	KCSS::loc_struct_t<int> x;
+	KCSS::loc_struct_t<node*> next;
+};
+
+void print_list(node* first_node){
+	node* next_node = first_node;
+	while(next_node != nullptr){
+		std::cout << kcss_instance.get(next_node->x) << "->";
+		next_node = kcss_instance.get(next_node->next);
+	}
+	std::cout << "nullptr\n\n";
+}
+
+void cleanup(node* first_node){
+	node* next_node = first_node;
+	node* following;
+	while(next_node != nullptr){
+		following = kcss_instance.get(next_node->next);
+		delete(next_node);
+		next_node = following;
+	}
+}
+
+
+void one_thread() {
+	
+	// Initial list: a(1) -> b(2) -> c(3) -> e(5)
+	node *e = new node(5, nullptr);
+	node *c = new node(3, e);
+	node *b = new node(2, c);
+	node *a = new node(1, b);
+
+	std::cout << "Creating initial list consisting of:\n";
+	print_list(a);
+	
+	// We want to delete node e(5). We will make use of KCSS where K = 2.
+	//  The first location involved is c->next, and the second one is e->next
+	std::cout << "Deleting node e(5)...\n";
+	// Apply kcss operation:
+	bool success = kcss_instance.kcss(c->next, e, (node*) nullptr, KCSS::mp(e->next, (node*) nullptr));
+	
+	if(success){ // Now it is safe to delete node e
+		std::cout << "Deletion operation successful. Current list:\n";
+		print_list(a);
+
+		delete(e);
+	}
+	else{ // Failure of kcss
+		std::cout << "Deletion operation unsuccessful. Current list:\n";
+		print_list(a);
+		
+		// ... (possibly retry) ...
+	}
+
+	// Perform the desired actions with the list (...)
+
+	cleanup(a);
+}
+
+void two_threads() {
+	
+	// Initial list: a(1) -> c(3) -> d(4)
+	node *d = new node(4, nullptr);
+	node *c = new node(3, d);
+	node *a = new node(1, c);
+
+	std::cout << "Creating initial list consisting of:\n";
+	print_list(a);
+	
+	// We want T1 to delete node d(4), while T2 inserts b(2). 
+	
+	auto delete_d = [&]() { // Will make use of KCSS where K = 2.
+		//  The first location involved is c->next, and the second one is d->next
+		std::cout << "Deleting node d(4)...\n";
+		// Apply kcss operation:
+		bool success = kcss_instance.kcss(c->next, d, (node*) nullptr, KCSS::mp(d->next, (node*) nullptr));
+		if(success){ // Now it is safe to delete node e
+			std::cout << "Deletion operation successful.\n";
+		}else{ // Failure of kcss
+			std::cout << "Deletion operation unsuccessful.\n";
+		}
+	};
+	auto insert_b = [&]() { // Will make use of KCSS where K = 1.
+		node *b = new node(2, c);
+		std::cout << "Inserting node b(2)...\n";
+		bool success = kcss_instance.kcss(a->next, c, b);
+		if(success){ // Now it is safe to delete node e
+			std::cout << "Insertion operation successful.\n";
+		}else{ // Failure of kcss
+			std::cout << "Insertion operation unsuccessful.\n";
+		}
+	};
+	
+	int num_threads = 2;
+	std::thread t[num_threads];
+	t[0] = std::thread(delete_d);
+	t[1] = std::thread(insert_b);
+	
+	t[0].join();
+	t[1].join();
+	
+	
+	std::cout << "Current list:\n";
+	print_list(a);
+	
+	// Perform the desired actions with the list (...)
+	
+	cleanup(a);
+}
+
+void n_threads(std::size_t n) {
+	
+	KCSS::loc_struct_t<int> v1(10);
+	KCSS::loc_struct_t<int> v2(20);
+	KCSS::loc_struct_t<int> v3(30);
 
 	auto f = [&]() {
 		while (true) {
-			int x = m.get(v1);
-			if (x > 1000000)
+			int x = kcss_instance.get(v1);
+			if (x > 100000)
 				break;
-			m.kcss(v1, x, x + 1, KCSS::mp(v2, 20), KCSS::mp(v3, 30));
-			// m.kcss(v1, x, x + 1);
+			kcss_instance.kcss(v1, x, x + 1, KCSS::mp(v2, 20), KCSS::mp(v3, 30));
 		}
 	};
 
 	std::thread t[n];
-	for (auto i = 0u; i < 5; i++)
+	for (auto i = 0u; i < n; i++)
 		t[i] = std::thread(f);
-	for (auto i = 0u; i < 5; i++)
+	for (auto i = 0u; i < n; i++)
 		t[i].join();
 
 
-//	std::cout << std::endl << "final value of v1 is: " << m.get(v1)
-//			<< std::endl;
+	std::cout << "\nThe final value of v1 is: " << kcss_instance.get(v1) << std::endl;
 
 }
 
+int main(){
 
-
-bool mutexcas(int *p, int expv, int newv, std::mutex &m) {
-	m.lock();
-	bool res = false;
-	if (*p == expv) {
-		*p = newv;
-		res = true;
-	}
-	m.unlock();
-	return res;
-}
-
-void test1(std::size_t n) {
-
-	std::mutex m;
-
-	int x = 10;
-
-	auto f = [&]() {
-		bool done = 0;
-		while (!done) {
-			if (x > 1000000)
-				break;
-			mutexcas(&x, x, x + 1, m);
-//			m.lock();
-//			if (y == 20 && z == 30) {
-//				x += 1;
-//			}
-//			m.unlock();
-		}
-	};
-
+	std::cout << "\nTest 1\n";
+	one_thread();
 	
-	std::thread t[n];
-	for (auto i = 0u; i < 5; i++)
-		t[i] = std::thread(f);
-	for (auto i = 0u; i < 5; i++)
-		t[i].join();
+	std::cout << "\nTest 2\n";
+	two_threads();
 
+	std::cout << "\nTest 3\n";
+	n_threads(6);
 
-//	std::cout << std::endl << "final value of x is: " << x << std::endl;
-
+	return 0;	
 }
 
-void foo() {
-	struct double_layout {
-		uint64_t mantisa :52;
-		uint64_t exp :11;
-		uint64_t sign :1;
-	};
-
-	union {
-		double d;
-		double_layout dl;
-	};
-
-	d = 0.123456789;
-	std::cout << dl.mantisa << std::endl;
-	std::cout << dl.exp << std::endl;
-	std::cout << dl.sign << std::endl;
-
-	KCSS::loc_t<double> v1;
-	uint64_t d1 = v1.to_value_t(d);
-	double d2 = v1.from_value_t(d1);
-	std::cout.precision(std::numeric_limits<double>::max_digits10);
-	std::cout << d << std::endl;
-	std::cout << d2 << std::endl;
-	std::cout << std::bitset<64>(*reinterpret_cast<uint64_t*>(&d)) << std::endl;
-	std::cout << std::bitset<64>(*reinterpret_cast<uint64_t*>(&d2))
-			<< std::endl;
-
-	if (d == d2)
-		std::cout << "Yes!" << std::endl;
-
-	long double bla = 1.3523345345345345;
-	std::cout << sizeof(bla) << " " << bla << std::endl;
-}
-
-void test_0() {
-	auto t1 = high_resolution_clock::now();
-	test0(20);
-	auto t2 = high_resolution_clock::now();
-	test1(20);
-	auto t3 = high_resolution_clock::now();
-
-	auto d1 = duration_cast<microseconds>(t2 - t1);
-	auto d2 = duration_cast<microseconds>(t3 - t2);
-	std::cout << d1.count() << std::endl;
-	std::cout << d2.count() << std::endl;
-	std::cout << d1.count() * 1.0 / d2.count() << std::endl;
-
-}
-
-void bar() {
-
-	KCSS m;
-
-	struct node {
-		node() :
-				x(0), next(nullptr) {
-		}
-		KCSS::loc_t<int> x;
-		KCSS::loc_t<node*> next;
-	};
-
-	node *n1 = new node();
-	node *n2 = new node();
-
-	m.kcss(n1->next, (node*) nullptr, n2);
-
-}
-
-int sum(int x) {
-	int s = 0;
-	for (int i = 0; i < x; ++i)
-		s += i;
-	return s;
-}
-
-// sum(5)  sum1<5>()
-
-template<int x>
-inline int sum1() {
-	return x + sum1<x - 1>();
-}
-
-template<>
-inline int sum1<0>() {
-	return 0;
-}
-
-
-
-template<typename ...Ts>
-void f(Ts &&...args) {
-}
-
-
-int main() {
-
-	int x;
-	f(x, 1, 2);
-
-	std::cout << sum1<10>() << std::endl;
-
-	struct ff {
-		int8_t v :7;
-		int8_t x :1;
-
-	};
-
-	ff a;
-	a.x = 0;
-	a.v = -65;
-
-	int8_t y = a.v;
-
-	std::cout << std::bitset<8>(-65) << std::endl;
-
-	std::cout << std::bitset<8>(*reinterpret_cast<int8_t*>(&a)) << std::endl;
-	std::cout << std::bitset<8>(y) << std::endl;
-
-	exit(0);
-	for (auto i = 0u; i < 10; ++i)
-		test_0();
-//	foo();
-}
-
+// ---------------------------------------------------------------------------------- //
